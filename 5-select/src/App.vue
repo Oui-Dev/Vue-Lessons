c6fca6a0f76aa25e154b8e9e78610ca884f50451
Kévin <kevin.blaise24@gmail.com>
2023-01-08 03:52:49 +0100

    feat(select) : setup vue + tailwind

---
+++ 5-select/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SelectMenu from './components/SelectMenu.vue';
+</script>
+
+<template>
+    <header class="flex items-center justify-center">
+        <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="80" height="80" />
+    </header>
+
+    <main class="flex flex-col items-center justify-center mt-12">
+        <SelectMenu />
+    </main>
+</template>
